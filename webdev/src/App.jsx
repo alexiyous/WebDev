694c9dc247105c524d84ce8de50a5bcd977524dc
Alexius Andrianno Alfa Renanta <alexiusrenanta@gmail.com>
2025-07-23 22:45:21 +0700

    Making Small Components

--- webdev/src/App.jsx
+++ webdev/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function Header({count, setCountFunction}) {
+  return (
+    <div>
+      <a href="https://vite.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite Logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React Logo" />
+      </a>
+      <div>
+          <h1>Learning React With Vite</h1>
+          <div className="card">
+            <button onClick={() => setCountFunction((count) => count + 1)}>
+              Test Clicking: {count}
+            </button>
+          </div>
+          <p className="read-the-docs">CLICK THOSE LOGOS AND FIND OUT 🤫</p>
+        </div>
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header count={count} setCountFunction={setCount} ></Header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
