216d9b415e42879dd8c093c31b6520e6ec9d471c
Alexius Andrianno Alfa Renanta <alexiusrenanta@gmail.com>
2025-07-26 01:05:51 +0700

    Dyanamic Value in JSX

--- webdev/src/App.jsx
+++ webdev/src/App.jsx
@@ -3,20 +3,30 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const wisdomPhrases = [
+  "React is a library for building user interfaces.",
+  "Vite is a build tool that aims to provide a faster and leaner development experience.",
+  "Components are the building blocks of React applications.",
+];
+
+function getRandomWisdom(length) {
+  return Math.floor(Math.random() * length);
+}
+
 function Header() {
   return (
     <>
-    <div>
-      <a href="https://vite.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite Logo" />
-      </a>
-      <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React Logo" />
-      </a>
       <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite Logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React Logo" />
+        </a>
+        <div>
           <h1>Learning React With Vite</h1>
         </div>
-    </div>
+      </div>
     </>
   );
 }
@@ -24,23 +34,25 @@ function Header() {
 function Description({ count, setCountFunction }) {
   return (
     <>
-     <div className="card">
-            <button onClick={() => setCountFunction((count) => count + 1)}>
-              Test Clicking: {count}
-            </button>
-          </div>
-          <p className="read-the-docs">CLICK THOSE LOGOS AND FIND OUT 🤫</p>
+      <div className="card">
+        <button onClick={() => setCountFunction((count) => count + 1)}>
+          Test Clicking: {count}
+        </button>
+      </div>
+      <div>
+        <p className="read-the-docs">CLICK THOSE LOGOS AND FIND OUT 🤫</p>
+        <h1>{wisdomPhrases[getRandomWisdom(wisdomPhrases.length)]}</h1>
+      </div>
     </>
-  )
+  );
 }
-
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header/>
+      <Header />
       <Description count={count} setCountFunction={setCount} />
     </>
   );
