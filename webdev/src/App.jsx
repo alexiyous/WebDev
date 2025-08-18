a94e6f9240a5e027949f313d406f0d4a1c9be039
Alexius Andrianno Alfa Renanta <alexiusrenanta@gmail.com>
2025-08-19 00:59:19 +0700

    Refactor logo styles and add example data for dynamic content display

--- webdev/src/App.jsx
+++ webdev/src/App.jsx
@@ -4,6 +4,7 @@ import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
 
 const wisdomPhrases = [
   "React is a library for building user interfaces.",
@@ -32,15 +33,16 @@ function Description({ count, setCountFunction }) {
 }
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState(null);
   const [count, setCount] = useState(0);
 
   function handleSelect(selected) {
-    console.log(selected);
+    setSelectedTopic(selected);
   }
 
   return (
     <>
-      <Header/>
+      <Header />
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
@@ -58,12 +60,25 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={()=> handleSelect("Components")}>Components</TabButton>
-            <TabButton onSelect={()=> handleSelect("JSX")}>JSX</TabButton>
-            <TabButton onSelect={()=> handleSelect("Props")}>Props</TabButton>
-            <TabButton onSelect={()=> handleSelect("State")}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          Dynamic Content
+          {/* can do this to !selectedTopic &&  <p>Please Select A Topic</p> */}
+          {!selectedTopic ? (
+            <p>Please Select A Topic</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
       <Description count={count} setCountFunction={setCount} />
