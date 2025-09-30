7c694b65311868d17b10c995bd82cdf5a361fe15
Alexius Andrianno Alfa Renanta <alexiusrenanta@gmail.com>
2025-10-01 02:17:45 +0700

    Refactor Player component to use initialName prop and implement editing functionality

--- tictactoe/src/components/Player.jsx
+++ tictactoe/src/components/Player.jsx
@@ -1,11 +1,30 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    // The best practice to use the previous value is not by directly
+    //  reading the isEditing, but instead do it like below
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value)
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? (
+          <input type="text" required value={playerName} onChange={handleChange}/>
+        ) : (
+          <span className="player-initialName">{playerName}</span>
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
